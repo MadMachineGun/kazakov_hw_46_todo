2f7ea30dc571320a9e03460fdc1bca402830e06b
Yurii Kazakov <gfnvortex@gmail.com>
2023-11-28 16:50:49 +0200

    Start

--- src/components/ToDo/index.js
+++ src/components/ToDo/index.js
@@ -1,21 +1,83 @@
-import {useState, useEffect} from 'react';
-import {useSelector} from 'react-redux';
-import {useDispatch} from 'react-redux';
+// import {useState, useEffect} from 'react';
+// import {useSelector} from 'react-redux';
+// import {useDispatch} from 'react-redux';
+//
+// import {addTodo, fetchTodos} from '../../store/slices/todoSlice';
+//
+// import Todo from './Todo';
+//
+// const TodoApp = () => {
+//     const dispatch = useDispatch();
+//
+//     const {loading, error, todoArray} = useSelector(state => state.todos)
+//
+//     const [newTodo, setNewTodo] = useState('');
+//
+//     useEffect(() => {
+//         dispatch(fetchTodos())
+//     }, []);
+//
+//     const addNewTodo = () => {
+//         if (newTodo.trim() !== '') {
+//             dispatch(addTodo(newTodo));
+//             setNewTodo('');
+//         }
+//     };
+//
+//     return (
+//         <div>
+//             <h1>Todo List</h1>
+//             <input type='checkbox'/>
+//             <input
+//                 type="text"
+//                 value={newTodo}
+//                 onChange={e => setNewTodo(e.target.value)}
+//                 placeholder="Enter a new todo"
+//             />
+//             <button onClick={addNewTodo}>Add Todo</button>
+//
+//             {loading && <h3>Loading....</h3>}
+//             {error && <h3>{error}</h3>}
+//
+//             <ul style={{
+//                 marginLeft: '15px',
+//                 marginTop: '20px',
+//                 display: 'flex',
+//                 flexDirection: 'column',
+//                 alignItems: 'flex-start',
+//                 justifyContent: 'center'
+//             }}>
+//                 {todoArray.map((todo, index) => (
+//                     <Todo
+//                         key={index}
+//                         todo={todo}
+//                         index={index}
+//                     />
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// };
+//
 
-import {addTodo, fetchTodos} from '../../store/slices/todoSlice';
+
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { addTodo, fetchTodos } from '../../store/slices/todoSlice';
 
 import Todo from './Todo';
 
 const TodoApp = () => {
     const dispatch = useDispatch();
 
-    const {loading, error, todoArray} = useSelector(state => state.todos)
+    const { loading, error, todoArray } = useSelector((state) => state.todos);
 
     const [newTodo, setNewTodo] = useState('');
 
     useEffect(() => {
-        dispatch(fetchTodos())
-    }, []);
+        dispatch(fetchTodos());
+    }, [dispatch]);
 
     const addNewTodo = () => {
         if (newTodo.trim() !== '') {
@@ -27,11 +89,11 @@ const TodoApp = () => {
     return (
         <div>
             <h1>Todo List</h1>
-            <input type='checkbox'/>
+            <input type="checkbox" />
             <input
                 type="text"
                 value={newTodo}
-                onChange={e => setNewTodo(e.target.value)}
+                onChange={(e) => setNewTodo(e.target.value)}
                 placeholder="Enter a new todo"
             />
             <button onClick={addNewTodo}>Add Todo</button>
@@ -39,20 +101,18 @@ const TodoApp = () => {
             {loading && <h3>Loading....</h3>}
             {error && <h3>{error}</h3>}
 
-            <ul style={{
-                marginLeft: '15px',
-                marginTop: '20px',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'flex-start',
-                justifyContent: 'center'
-            }}>
+            <ul
+                style={{
+                    marginLeft: '15px',
+                    marginTop: '20px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'flex-start',
+                    justifyContent: 'center',
+                }}
+            >
                 {todoArray.map((todo, index) => (
-                    <Todo
-                        key={index}
-                        todo={todo}
-                        index={index}
-                    />
+                    <Todo key={index} todo={todo.text} index={index} />
                 ))}
             </ul>
         </div>
--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -3,7 +3,7 @@ import {createSlice} from '@reduxjs/toolkit';
 const userSlice = createSlice({
   name: 'user',
   initialState: {
-    userName: 'Ihor',
+    userName: 'Enter your Name',
   },
   reducers: {
     changeName(state, action) {
